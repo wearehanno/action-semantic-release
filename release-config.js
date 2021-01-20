252120891b8264a758827f5d2bae90c40d24c34b
Marcel Kalveram <64201204+hanno-marcelkalveram@users.noreply.github.com>
2021-01-20 15:28:18 +0100

    first commit

---
+++ release-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'docs/CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        tarballDir: 'pack',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: [{ path: 'pack/*.tgz', label: 'Release' }],
+      },
+    ],
+  ],
+};
