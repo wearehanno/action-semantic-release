cf10e5e688eee4b3c1cf2fd0be5fc4762700c5c0
Marcel Kalveram <64201204+hanno-marcelkalveram@users.noreply.github.com>
2021-01-21 11:35:04 +0100

    fix: reenable NPM plugin

--- release-config.js
+++ release-config.js
@@ -10,6 +10,13 @@ module.exports = {
       },
     ],
     [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+        tarballDir: 'pack',
+      },
+    ],
+    [
       '@semantic-release/github',
       {
         assets: [{ path: 'pack/*.tgz', label: 'Release' }],
